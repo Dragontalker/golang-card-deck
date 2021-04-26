fe23b5cf6c4d10cb34e8805f0a923d43ab100477
Richard <richard.yang.tong@gmail.com>
2021-04-25 20:48:15 -0400

    Added two slices for suits and values of cards.

--- deck.go
+++ deck.go
@@ -6,6 +6,34 @@ import "fmt"
 // which is a slice of strings
 type deck []string
 
+func newDeck() deck {
+	cards := deck{}
+
+	cardSuits := []string{
+		"Spades",
+		"Diamonds",
+		"Hearts",
+		"Clubs",
+	}
+
+	cardValues := []string{
+		"Ace",
+		"Two",
+		"Three",
+		"Four",
+		"Five",
+		"Six",
+		"Seven",
+		"Eight",
+		"Nine",
+		"Ten",
+		"Jack",
+		"Queen",
+		"King",
+	}
+
+}
+
 func (d deck) print() {
 	for i, card := range d {
 		fmt.Println(i, card)
